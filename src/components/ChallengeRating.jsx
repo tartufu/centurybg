0fc33c738a3bd847b30b164b93f096f355691fa3
tartufu <ainurlindale@gmail.com>
2021-01-27 15:04:32 +0800

    added API call to update playcount & challenge rating

--- src/components/ChallengeRating.jsx
+++ src/components/ChallengeRating.jsx
@@ -1,11 +1,23 @@
 import React, { useState } from 'react';
 import { changeChallengeRatingAction } from '../action/challengeRatingAction';
-import {useSelector, useDispatch} from 'react-redux';
+import {useSelector, useDispatch, useStore} from 'react-redux';
+import axios from 'axios';
 
-export default function ChallengeRating({ store }) {
+
+export default function ChallengeRating() {
 
     const challengeRating = useSelector(state => state.challengeRating);
+    const globalState = useSelector(state => state);
     const dispatch = useDispatch();
+
+    const postRequest = () => {
+        axios.post('http://localhost:3000/users/update', globalState
+        ).then( (res) => {
+            // console.log(res)
+        })
+
+        // console.log("global", globalState);
+    };
 
     return (
         <div>
@@ -13,6 +25,7 @@ export default function ChallengeRating({ store }) {
             <button onClick={() => {dispatch(changeChallengeRatingAction(1))}}> 1 x 1 </button>
             <button onClick={() => {dispatch(changeChallengeRatingAction(5))}}> 5 x 5 </button>
             <button onClick={() => {dispatch(changeChallengeRatingAction(10))}}> 10 x 10 </button>
+            <button onClick={postRequest}> UPDATE POST REQUEST </button>
         </div>
     )
 }
